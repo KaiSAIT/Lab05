8aedc247311be2856c293d2b4e864d7c386a620b
849533 <849533@SAIT227692IT.ACDM.DS.SAIT.CA>
2021-12-09 08:12:04 -0700

    LoginServlet.java -added messages on invalid session and logout -updated code for logging in

--- Lab05SessionAuthentication/src/java/servlets/LoginServlet.java
+++ Lab05SessionAuthentication/src/java/servlets/LoginServlet.java
@@ -2,26 +2,39 @@ package servlets;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import models.User;
 import services.AccountService;
-import utilities.CookieUtil;
 
 public class LoginServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        
         HttpSession session = request.getSession();
-        session.invalidate();
-
-        Cookie[] cookies = request.getCookies();
-        String username = CookieUtil.getCookieValue(cookies, "username");
-        request.setAttribute("cookies", cookies);
+        
+        String logout = "";
+        logout = request.getParameter("logout");
+        
+        if ( logout != null ) {
+            
+            session.invalidate();
+            request.setAttribute("logoutMessage", true);
+            getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+            return;
+            
+        }
+        
+        if ( session.getAttribute("username") != null ) {
+            
+            response.sendRedirect("home"); 
+            return;
+            
+        }
 
         getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
         return;
@@ -30,29 +43,42 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        
+        HttpSession session = request.getSession();
+        
         String username = "";
         String password = "";
         username = request.getParameter("userName");
         password = request.getParameter("password");
 
-        Cookie cookie = new Cookie("username", username);
-        response.addCookie(cookie);
-
         AccountService as = new AccountService();
 
         User user = as.login(username, password);
 
-        request.setAttribute("user", user);
-
-        if (username == null || username.equals("") || password == null || password.equals("")) {
-            request.setAttribute("invalidEntry", true);
-            getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
-            return;
-        } else {
-            HttpSession session = request.getSession();
-            session.setAttribute("username", username);
-            response.sendRedirect("home");
+        if ( username == null || username.equals("") || password == null || password.equals("") ) {
+            
+            user = user.login(username, password);
+            
+            if ( user != null ) {
+                
+                session.setAttribute("username", user.getUsername());
+                response.sendRedirect("home");
+                return;
+                
+            } 
+            
+            else {
+                
+                session.setAttribute("username", username);
+                session.setAttribute("password", password);
+                session.setAttribute("invalidMessage", true);
+                response.sendRedirect("login");
+                return;
+                
+            }
+            
         }
+        
     }
 
 }
