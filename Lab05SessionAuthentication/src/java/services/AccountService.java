96c68ecf0d0bd1d35d02ae7ec6925ac6ebd572e2
849533 <849533@SAIT227692IT.ACDM.DS.SAIT.CA>
2021-12-09 08:10:21 -0700

    AccountService.java -redid AccountService.java and hardcoded in accounts

---
+++ Lab05SessionAuthentication/src/java/services/AccountService.java
@@ -0,0 +1,17 @@
+package services;
+
+import models.User;
+
+public class AccountService {
+    
+    public User login(String username, String password) {
+        
+        if ( (username.equals("Abe") || username.equals("Barb")) && password.equals("password") ) {
+            
+            return new User(username, null);
+            
+        }
+        
+        return null;
+    }
+}
